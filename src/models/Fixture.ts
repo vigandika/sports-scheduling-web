6a6fc27e0431b8cbd5b87a193157d31b825eb4bb
Vigan Dika <vigan.dika@studentet.uni-pr.edu>
2021-12-20 21:36:47 +0100

    Model Matchweeks response

---
+++ src/models/Fixture.ts
@@ -0,0 +1,4 @@
+export class Fixture {
+    private homeTeam?: string;
+    private awayTeam?: string;
+}
---
+++ src/models/Matchweek.ts
@@ -0,0 +1,5 @@
+import { Fixture } from "./Fixture";
+
+export class Matchweek {
+    private fixtures?: Array<Fixture>;
+}
